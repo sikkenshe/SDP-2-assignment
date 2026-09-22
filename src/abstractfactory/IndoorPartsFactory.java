744693388afdd0cdf616de2335cb2010248c2f9e
Bekarys Bekkazy <253125@astanait.edu.kz>
2026-09-22 14:46:53 +0500

    add concrete robot parts factories

--- src/abstractfactory/IndoorPartsFactory.java
+++ src/abstractfactory/IndoorPartsFactory.java
@@ -1,4 +1,14 @@
 package abstractfactory;
 
-public class IndoorPartsFactory {
+public class IndoorPartsFactory implements RobotPartsFactory {
+
+    @Override
+    public Sensor createSensor() {
+        return new IndoorSensor();
+    }
+
+    @Override
+    public Battery createBattery() {
+        return new IndoorBattery();
+    }
 }
--- src/abstractfactory/OutdoorPartsFactory.java
+++ src/abstractfactory/OutdoorPartsFactory.java
@@ -1,4 +1,14 @@
 package abstractfactory;
 
-public class OutdoorPartsFactory {
+public class OutdoorPartsFactory implements RobotPartsFactory {
+
+    @Override
+    public Sensor createSensor() {
+        return new OutdoorSensor();
+    }
+
+    @Override
+    public Battery createBattery() {
+        return new OutdoorBattery();
+    }
 }
