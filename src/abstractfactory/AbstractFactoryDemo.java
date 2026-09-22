0b5f56dcfa0e9637bfc68647fbded6318ec211cb
Bekarys Bekkazy <253125@astanait.edu.kz>
2026-09-22 14:48:09 +0500

    add abstract factory demo

--- src/abstractfactory/AbstractFactoryDemo.java
+++ src/abstractfactory/AbstractFactoryDemo.java
@@ -1,4 +1,12 @@
 package abstractfactory;
 
 public class AbstractFactoryDemo {
+
+    public static void main(String[] args) {
+        RobotPartsFactory indoorFactory = new IndoorPartsFactory();
+
+        RobotWorkshopClient client = new RobotWorkshopClient(indoorFactory);
+
+        client.buildRobot();
+    }
 }
