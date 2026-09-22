ecd955ffc05ee593cc964ae03b08bca0a0f86c66
Bekarys Bekkazy <253125@astanait.edu.kz>
2026-09-22 14:47:11 +0500

    add robot workshop client

--- src/abstractfactory/RobotWorkshopClient.java
+++ src/abstractfactory/RobotWorkshopClient.java
@@ -1,4 +1,18 @@
 package abstractfactory;
 
 public class RobotWorkshopClient {
+
+    private final RobotPartsFactory factory;
+
+    public RobotWorkshopClient(RobotPartsFactory factory) {
+        this.factory = factory;
+    }
+
+    public void buildRobot() {
+        Sensor sensor = factory.createSensor();
+        Battery battery = factory.createBattery();
+
+        sensor.scan();
+        battery.charge();
+    }
 }
